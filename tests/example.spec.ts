1d0afa218bfbcbc941c5f061cb6efe99126146b2
Nicolas Grossi <nicolas.grossi@gmail.com>
2023-03-06 14:45:34 -0300

    updated tests with .isVisible instead of .click

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,23 @@
 import { test, expect } from '@playwright/test';
 const URL = 'https://www.atlassian.com/';
+const URLClarin = 'http://www.clarin.com';
 
-test('test', async ({ page }) => {
+
+test('test Atlassian', async ({ page }) => {
   await page.goto(URL);
-  await page.getByText('with teamwork').click();
-  await page.getByRole('heading', { name: 'It’s possible with teamwork' }).click();
-  await page.getByRole('link', { name: 'Products', exact: true }).click();
-  await page.getByRole('link', { name: 'For teams' }).click();
-  await page.getByRole('link', { name: 'Support', exact: true }).click();
+  await page.getByText('with teamwork').click() 
+  page.getByRole('heading', { name: 'It’s possible with teamwork' }).isVisible
+  page.getByRole('link', { name: 'Products', exact: true }).isVisible;
+  page.getByRole('link', { name: 'For teams' }).isVisible
+  page.getByRole('link', { name: 'Support', exact: true }).isVisible
+});
+
+
+test('test Clarin', async ({ page }) => {
+  await page.goto(URLClarin);
+  page.getByRole('link', { name: 'Último Momento' }).nth(1).isVisible
+  page.getByRole('link', { name: 'Política' }).nth(1).isVisible
+  page.getByRole('link', { name: 'Claringrilla' }).nth(1).isVisible
 });
 
 
