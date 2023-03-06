b8602c6144891dbe6ed6f8525835cf87d23af79d
Nicolas Grossi <nicolas.grossi@gmail.com>
2023-03-06 14:05:19 -0300

    removed duplicated test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,7 +5,6 @@ test('test', async ({ page }) => {
   await page.goto(URL);
   await page.getByText('with teamwork').click();
   await page.getByRole('heading', { name: 'It’s possible with teamwork' }).click();
-  await page.getByRole('heading', { name: 'It’s possible with teamwork' }).click();
   await page.getByRole('link', { name: 'Products', exact: true }).click();
   await page.getByRole('link', { name: 'For teams' }).click();
   await page.getByRole('link', { name: 'Support', exact: true }).click();
