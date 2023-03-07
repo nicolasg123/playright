6325afbc15e4a2418c4a5eb6cea057a873990517
Nicolas Grossi <nicolas.grossi@gmail.com>
2023-03-07 18:54:44 -0300

    added a linkedin login test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 const URL = 'https://www.atlassian.com/';
 const URLClarin = 'http://www.clarin.com';
+const URLLinkedin = 'http://www.linkedin.com';
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
+const EMAIL = 'nicolas.grossi@gmail.com';
+
 
 
 test('test Atlassian', async ({ page }) => {
@@ -24,7 +28,23 @@ test('test Clarin2', async ({ page }) => {
   await page.goto(URLClarin);
   await page.click('svg#Capa_1')
   await page.click('span.mount')
-  const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 // Sleeps for 2 seconds.
   await sleep(2000);
+});
+
+test('test LINKEDIN', async ({ page }) => {
+  await page.goto(URLLinkedin);
+  await sleep(2000);
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await sleep(2000);
+  await page.getByLabel('Email or Phone').click();
+  await sleep(2000);
+  await page.getByLabel('Email or Phone').fill(EMAIL);
+  await sleep(2000);
+  await page.getByLabel('Password').click();
+  await sleep(2000);
+  await page.getByLabel('Password').fill(PASSWORD);
+  await sleep(2000);
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await sleep(5000);
 });
